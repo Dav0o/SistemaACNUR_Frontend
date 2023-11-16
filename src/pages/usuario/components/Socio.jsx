c5729349110bb0d2f6b5d871e3aa2a43c4d0dad1
Rosi-07 <cuberoflores07@gmail.com>
2023-11-15 18:47:13 -0600

    add functionality in socio

--- src/pages/usuario/components/Socio.jsx
+++ src/pages/usuario/components/Socio.jsx
@@ -1,70 +1,258 @@
 import { useState, useEffect, useRef } from "react"
-import { useParams, Link } from "react-router-dom";
-import { Button, Table, Modal, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Button, Table, Form } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-
-
-const socios = [
-    {
-        id: 1,
-        name: "Martha",
-        lastName1: "Perez",
-        lastName2: "Gonzalez",
-        cuentaBancaria: "2613817391",
-        phone: "12345678"
-      
-    },
-    {
-        id: 2,
-        name: "Rogelio",
-        lastName1: "Gomez",
-        lastName2: "Pereira",
-        cuentaBancaria: "663628261",
-        phone: "12345678",
-     
-    },
-]
+import api from "../../../api/axios";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Accordion from "react-bootstrap/Accordion";
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
+import { Modal } from "react-bootstrap";
 
 function Socio() {
 
-    const LinkStyle = {
-        textDecoration: "none",
-        color: "white",
-      };
+  const idSocio = useRef(null);
+  const cuentaBanco = useRef(null);
+  const usuarioDni = useRef(null);
+  const cuotaId = useRef(null);
+
+  const [socios, setSocios] = useState([]);
+  const [usuarios, setUsuarios] = useState([]);
+  const [cuotas, setCuotas] = useState([]);
+
+  const [refresh, setRefresh] = useState(false);
+
+  useEffect(() => {
+
+    api.get("Usuario")
+      .then((response) => {
+        setUsuarios(response.data);
+      })
+      .catch((error) => {
+        console.error('Error :', error);
+      });
+
+
+    api.get("Cuotas")
+      .then((response) => {
+        setCuotas(response.data);
+      })
+      .catch((error) => {
+        console.error('Error :', error);
+      });
+  }, []);
+
+
+  useEffect(() => {
+    api
+      .get("Socios")
+      .then((response) => {
+        setSocios(response.data);
+      })
+      .catch((error) => {
+        console.error("Error al obtener datos:", error);
+      });
+  }, [refresh]);
+
+  const handleSave = () => {
+
+    let newSocio = {
+      idSocio: idSocio.current.value,
+      cuentaBanco: cuentaBanco.current.value,
+      usuarioDni: usuarioDni.current.value,
+      cuotaId: cuotaId.current.value
+    };
+    console.log("datos:", newSocio);
+    api
+      .post("Socios", newSocio)
+      .then((response) => {
+        console.log(response);
+        setRefresh(!refresh);
+        Swal.fire({
+          title: 'Éxito',
+          text: '¡Socio registrado exitosamente!',
+          icon: 'success',
+        });
+        
+        idSocio.current.value = '';
+        cuentaBanco.current.value = '';
+        usuarioDni.current.value = '';
+        cuotaId.current.value = '';
+      })
+      .catch((error) => {
+        console.log(error);
+        Swal.fire({
+          title: 'Error',
+          text: 'Ocurrió un error al registrar al socio.',
+          icon: 'error',
+        });
+      });
+  };
+
+  const LinkStyle = {
+    textDecoration: "none",
+    color: "white",
+  };
+
+
+  //////////////////////----------update------------///////////////////////////7
+  const [editingSocio, setEditingSocio] = useState(null);
+  const [showEditModal, setShowEditModal] = useState(false);
+
+  const handleEditClick = (socio) => {
+    setEditingSocio(socio);
+    setShowEditModal(true);
+  };
+
+  const handleCloseEditModal = () => {
+    setShowEditModal(false);
+    setEditingSocio(null);
+  };
+
+
+  const handleUpdate = () => {
+
+    const updatedSocio = {
+      idSocio: idSocio.current.value,
+      cuentaBanco: cuentaBanco.current.value,
+      usuarioDni: usuarioDni.current.value,
+      cuotaId: cuotaId.current.value
+    };
+
+    api
+      .put("Socios", updatedSocio)
+      .then((response) => {
+        Swal.fire({
+          title: "¡Actualizado!",
+          text: "El socio ha sido actualizado correctamente.",
+          icon: "success",
+          confirmButtonColor: "#3085d6",
+        }).then(() => {
+          console.log(response);
+          setShowEditModal(false);
+          setEditingSocio(null);
+        });
+      })
+      .catch((error) => {
+        console.error("Error al actualizar el socio:", error);
+        Swal.fire({
+          title: "Error",
+          text: "Hubo un error al actualizar el socio.",
+          icon: "error",
+          confirmButtonColor: "#d33",
+        });
+      });
+  };
+  ///////////////////---------------------------------//////////////////////////////
+
+
+
+  //////////////////////----------delete------------///////////////////////////7
+
+  const handleDelete = (cuotaId) => {
+    Swal.fire({
+      title: "¿Estás seguro?",
+      text: "¡No podrás revertir esto!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#d33",
+      cancelButtonColor: "#3085d6",
+      confirmButtonText: "Sí, eliminarlo",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        api
+          .delete(`Socios?Id=${cuotaId}`)
+          .then((response) => {
+            console.log(response);
+
+            const updatedSocios = socios.filter(
+              (socio) => socio.idSocio !== idSocio
+            );
+            setSocios(updatedSocios);
+          })
+          .catch((error) => {
+            console.error("Error al eliminar el socio:", error);
+          });
+      }
+    });
+  };
+  ///////////////////---------------------------------//////////////////////////////
+
 
 
   return (
-    <>
+
     <Container className="container-fluid">
-      <h1 className="h3 mb-2 text-gray-800">
-        Socios
-      </h1>
+      <h1 className="h3 mb-2 text-gray-800"> Socios  </h1>
       <p class="mb-4">Lista de los socios</p>
+
       <div className="card shadow mb-4">
-        <div className="card-header py-3">
-          <div className="d-flex justify-content-between">
-            <div>Clik en el botón para crear un nuevo socio</div>
-            <Button
-              variant="success"
-              className="bg-gradient-success text-light
-              "
-              onClick={/* handleShowFormModal */ 1+1}
-            >
-              <i class="bi bi-plus-square"></i>
-            </Button>
-          </div>
-        </div>
+        <Accordion defaultActiveKey="1">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>
+              Click en el botón para crear un nuevo socio
+            </Accordion.Header>
+            <Accordion.Body>
+              <Container>
+                <Row className="mb-2">
+                  <Col>
+                    <Form.Label htmlFor="inputIdSocio">Id Socio</Form.Label>
+                    <Form.Control type="text" id="inputIdSocio" ref={idSocio} />
+                  </Col>
+
+                  <Col>
+                    <Form.Label htmlFor="inputUsuarioDni">Usuario</Form.Label>
+                    <Form.Control as="select" id="inputUsuarioDni" ref={usuarioDni}>
+                      <option value="">Seleccione un usuario</option>
+                      {usuarios.map((usuario) => (
+                        <option key={usuario.DNI} value={usuario.dniUsuario}>
+                          {usuario.dniUsuario} - {usuario.nombreUsuario} {usuario.apellido1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+
+                  <Col>
+                    <Form.Label htmlFor="inputCuentaBanco">Cuenta Banco</Form.Label>
+                    <Form.Control type="text" id="inputCuentaBanco" ref={cuentaBanco} />
+                  </Col>
+
+                  <Col>
+                    <Form.Label htmlFor="inputCuotaId">Cuota</Form.Label>
+                    <Form.Control as="select" id="inputCuotaId" ref={cuotaId}>
+                      <option value="">Seleccione una cuota</option>
+
+                      {cuotas.map((cuota) => (
+                        <option key={cuota.idCuota} value={cuota.idCuota}>
+                          <option value="{cuota.idCuota}"> {cuota.tipoCuota} -${cuota.cargosAnuales}</option>
+
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                </Row>
+                <Button variant="primary" onClick={handleSave} className="mt-3">
+                  Guardar
+                </Button>
+              </Container>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+
+
         <div className="card-body">
           <Table striped="columns">
             <thead>
               <tr>
-                  <th>ID</th>
-                <th>Nombre</th>
-                <th>Apellidos</th>
+                <th>ID</th>
+                <th>DniUsuario</th>
                 <th>Cuenta Bancaria</th>
-                <th>Telefono</th>
+                <th>Cuota</th>
                 <th>Acciones</th>
               </tr>
             </thead>
@@ -72,37 +260,94 @@ function Socio() {
             <tbody>
               {socios.map((socio) => (
                 <tr key={socio.id}>
-                  <td>{socio.id}</td>
-                  <td>{socio.name}</td>
-                  <td>{socio.lastName1} {socio.lastName2}</td>
-                  <td>{socio.cuentaBancaria}</td>
-                  <td>{socio.phone}</td>
-                
+                  <td>{socio.idSocio}</td>
+                  <td>{socio.usuarioDni}</td>
+                  <td>{socio.cuentaBanco}</td>
+                  <td>{socio.cuotaId}</td>
+
                   <td>
+
                     <Button
                       variant="warning"
                       className="bg-gradient-warning mr-1 text-light"
-                      onClick={/* () => handleEditClick(voluntSanitario.id) */ 1+1}
+                      onClick={() => handleEditClick(socio)}
                     >
                       <i class="bi bi-pencil-square"></i>
                     </Button>{" "}
                     <Button
                       variant="danger"
                       className="bg-gradient-danger mr-1 text-light"
-                      onClick={/* () => handleOpenModal(voluntSanitario.id) */  1+1}
+                      onClick={() => handleDelete(socio.idSocio)}
                     >
-                     <i class="bi bi-trash"></i>
+                      <i class="bi bi-trash"></i>
+
                     </Button>{" "}
 
-                    
+
                   </td>
                 </tr>
               ))}
             </tbody>
-            
+
           </Table>
         </div>
       </div>
+
+      <Modal show={showEditModal} onHide={handleCloseEditModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Actualizar Alimento</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group>
+              <Form.Label>Id Socio</Form.Label>
+              <Form.Control
+                type="text"
+                defaultValue={editingSocio ? editingSocio.idSocio : ''}
+                ref={idSocio}
+                readOnly
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Cuenta de Banco</Form.Label>
+              <Form.Control
+                type="text"
+                defaultValue={editingSocio ? editingSocio.cuentaBanco : ''}
+                ref={cuentaBanco}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Dni Usuario</Form.Label>
+              <Form.Control
+                type="text"
+                defaultValue={editingSocio ? editingSocio.usuarioDni : ''}
+                ref={usuarioDni}
+                readOnly
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Id Cuota</Form.Label>
+              <Form.Control as="select" ref={cuotaId}>
+            
+                {cuotas.map((cuota) => (
+                  <option key={cuota.idCuota} value={cuota.idCuota} selected={editingSocio?.cuotaId === cuota.idCuota}>
+                    {cuota.idCuota}
+                  </option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={handleCloseEditModal}>
+            Cancelar
+          </Button>
+          <Button variant="success" onClick={handleUpdate}>
+            Actualizar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
 
       <Link style={LinkStyle} to={"/usuario"}>
         <Button variant="dark" className="bg-gradient-danger">
@@ -111,186 +356,6 @@ function Socio() {
       </Link>
     </Container>
 
-    {/* <Modal show={modalCreate} onHide={handleCloseFormModal} centered>
-      <Modal.Header closeButton>
-        <Modal.Title>Crear voluntario sanitario </Modal.Title>
-      </Modal.Header>
-
-      <Modal.Body>
-        <Form>
-          <div className="row">
-            <div className="col-md-6">
-              <Form.Group controlId="formPlaca">
-                <Form.Label>Nombre</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Ingrese el daño"
-                  ref={name}
-                />
-              </Form.Group>
-
-              <Form.Group controlId="formPlaca">
-                <Form.Label>Fecha</Form.Label>
-                <Form.Control
-                  type="date"
-                  placeholder="Fecha de cuando sucedió"
-                  ref={date}
-                />
-              </Form.Group>
-            </div>
-            <div className="col-md-6">
-              <Form.Group controlId="formColor">
-                <Form.Label>Gravedad</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Ingrese la gravedad"
-                  ref={severity}
-                />
-              </Form.Group>
-
-              <Form.Group controlId="formColor">
-                <Form.Label>Tipo</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Ingrese el tipo"
-                  ref={type}
-                />
-              </Form.Group>
-            </div>
-          </div>
-          <Form.Group controlId="formColor">
-            <Form.Label>Descripción</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Ingrese la descripción del suceso"
-              ref={description}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Imagen</Form.Label>
-            <div className="custom-file">
-              <input
-                type="file"
-                className="custom-file-input"
-                id="customFile"
-               
-                onChange={handleImageUpload}
-              />
-              <label className="custom-file-label" htmlFor="customFile">
-              </label>
-            </div>
-            {imageUrl && <img src={imageUrl} alt="Imagen subida" className="uploadedImg"
-            style={{ maxWidth: '200px', maxHeight: '200px' }} />}
-          </Form.Group>
-        </Form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="danger" onClick={handleCloseFormModal}>
-          Cancelar
-        </Button>
-        <Button variant="dark" onClick={handleSave}>
-          Guardar
-        </Button>
-      </Modal.Footer>
-    </Modal>
-
-    <Modal show={showEditModal} onHide={handleCloseEditModal} centered>
-      <Modal.Header closeButton>
-        <Modal.Title>Editar mantenimiento</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form>
-          <div className="row">
-            <div className="col-md-6">
-              <Form.Group controlId="formName">
-                <Form.Label>Nombre</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Ingrese el nombre"
-                  defaultValue={
-                    editingMaintenance ? editingMaintenance.name : ""
-                  }
-                  ref={name}
-                />
-              </Form.Group>
-
-              <Form.Group controlId="formCategory">
-                <Form.Label>Categoría</Form.Label>
-                <Form.Control
-                  type="number"
-                  placeholder="Ingrese la categoría"
-                  defaultValue={
-                    editingMaintenance ? editingMaintenance.category : 0
-                  }
-                  ref={category}
-                />
-              </Form.Group>
-            </div>
-            <div className="col-md-6">
-              <Form.Group controlId="formSeverity">
-                <Form.Label>Gravedad</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Ingrese la gravedad"
-                  defaultValue={
-                    editingMaintenance ? editingMaintenance.severity : ""
-                  }
-                  ref={severity}
-                />
-              </Form.Group>
-
-              <Form.Group controlId="formType">
-                <Form.Label>Tipo</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Ingrese el tipo"
-                  defaultValue={
-                    editingMaintenance ? editingMaintenance.type : ""
-                  }
-                  ref={type}
-                />
-              </Form.Group>
-            </div>
-          </div>
-          <Form.Group controlId="formDescription">
-            <Form.Label>Descripción</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Ingrese la descripción"
-              defaultValue={
-                editingMaintenance ? editingMaintenance.description : ""
-              }
-              ref={description}
-            />
-          </Form.Group>
-        </Form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="danger" onClick={handleCloseEditModal}>
-          Cancelar
-        </Button>
-        <Button variant="dark" onClick={handleUpdate}>
-          Actualizar
-        </Button>
-      </Modal.Footer>
-    </Modal>
-    <Modal show={showModal} onHide={handleCloseModal}>
-      <Modal.Header closeButton>
-        <Modal.Title>Confirmar eliminación</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        ¿Estás seguro de que quieres eliminar este mantenimiento?
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleCloseModal}>
-          Cancelar
-        </Button>
-        <Button variant="danger" onClick={handleDeleteMaintenance}>
-          Eliminar
-        </Button>
-      </Modal.Footer>
-    </Modal> */}
-  </>
   )
 }
 
